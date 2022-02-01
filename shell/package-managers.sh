cf65d7c99ee61668a200ee8e3a5f040134cfb58f
Yuri Pereira Constante <ypconstante@gmail.com>
2022-01-31 23:44:24 -0300

    Add package managers script

---
+++ shell/package-managers.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
+
+my:step-begin "setup flatpak"
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+my:step-begin "setup dnf"
+my:append-to-file-if-not-contains /etc/dnf/dnf.conf 'max_parallel_downloads=10'
+
+my:step-begin "add rpm fusion"
+RELEASE_NUMBER=$(rpm -E %fedora)
+my:dnf-install \
+    "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE_NUMBER}.noarch.rpm" \
+    "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE_NUMBER}.noarch.rpm"
+
+my:step-begin "enable rpm fusion appstream metadata"
+sudo dnf -y groupupdate core
