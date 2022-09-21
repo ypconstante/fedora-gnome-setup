869ae00ad6750f5654dfa26569e1333118d5e315
Yuri Pereira Constante <ypconstante@gmail.com>
2022-09-20 21:53:10 -0300

    Install node 18

--- shell/dev_node.sh
+++ shell/dev_node.sh
@@ -4,8 +4,8 @@ source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
 
 my:step-begin "install"
 my:asdf-add-plugin nodejs
-my:asdf-install-and-set-global nodejs latest:14
 my:asdf-install-and-set-global nodejs latest:16
+my:asdf-install-and-set-global nodejs latest:18
 
 my:step-begin "configure node"
 my:append-to-file-if-not-contains "$NPM_CONFIG_USERCONFIG" 'engine-strict = true'
