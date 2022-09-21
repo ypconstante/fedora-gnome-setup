866c7caac2a39eeafe208f770b167655ebb49b54
Yuri Pereira Constante <ypconstante@gmail.com>
2022-09-20 22:05:24 -0300

    Remove desktop config

--- shell/desktop.sh
+++ shell/desktop.sh
@@ -15,22 +15,3 @@ install-extension() {
 
 my:step-begin "install extension manager"
 my:flatpak-install com.mattjakeman.ExtensionManager
-
-my:step-begin "install custom extension"
-TMP_DIR=$(mktemp -d)
-gnome-extensions pack "$ASSETS_DIR/desktop--extension" -o "$TMP_DIR"
-gnome-extensions install "$TMP_DIR/setup@ypconstante.github.com.shell-extension.zip"
-
-my:step-begin "install extensions"
-install-extension "AlphabeticalAppGrid@stuarthayhurst"
-install-extension "blur-my-shell@aunetx"
-install-extension "dash-to-panel@jderose9.github.com"
-install-extension "just-perfection-desktop@just-perfection"
-install-extension "sound-output-device-chooser@kgshank.net"
-
-my:step-begin "disable default extensions"
-gnome-extensions disable background-logo@fedorahosted.org
-
-my:step-begin "load config"
-dconf load / < "$ASSETS_DIR/desktop--config.dconf"
-
