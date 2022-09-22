993a39b0cee969d366d4cd83761f4e6a13763455
Yuri Pereira Constante <ypconstante@gmail.com>
2022-09-22 00:18:31 -0300

    Add copr for starship

--- shell/terminal_starship.sh
+++ shell/terminal_starship.sh
@@ -3,5 +3,6 @@
 source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
 
 my:step-begin "install"
+sudo dnf copr enable -y atim/starship
 my:dnf-install starship
 my:link-file "$ASSETS_DIR/terminal_starship.toml" "$XDG_CONFIG_HOME/starship.toml"
