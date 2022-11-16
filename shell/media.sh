e68d9e4345e4dad03ab4a15b27d79fcf0ae5918e
Yuri Pereira Constante <ypconstante@gmail.com>
2022-11-16 01:06:39 -0300

    Use celluloid as media player

--- shell/media.sh
+++ shell/media.sh
@@ -2,5 +2,8 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
 
-my:step-begin "install vlc"
-my:dnf-install vlc
+my:step-begin "install celluloid"
+my:dnf-install celluloid --setopt=install_weak_deps=False
+
+my:step-begin "remove gnome videos"
+ my:dnf-remove totem
