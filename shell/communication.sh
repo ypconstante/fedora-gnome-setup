8d561ebb8cce47ae22e58111ee3f845c7dab393a
Yuri Pereira Constante <ypconstante@gmail.com>
2022-11-17 23:09:56 -0300

    Remove slack install

--- shell/communication.sh
+++ shell/communication.sh
@@ -2,22 +2,10 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
 
-SLACK_APP_NAME=com.slack.Slack
 ZOOM_APP_NAME=us.zoom.Zoom
 
 my:step-begin "install telegram"
 my:flatpak-install org.telegram.desktop
-
-my:step-begin "install slack"
-my:flatpak-install $SLACK_APP_NAME
-
-my:step-begin "configure slack"
-flatpak override --user $SLACK_APP_NAME --reset
-flatpak override --user $SLACK_APP_NAME \
-    --nofilesystem=xdg-documents \
-    --nofilesystem=xdg-music \
-    --nofilesystem=xdg-pictures \
-    --nofilesystem=xdg-videos
 
 my:step-begin "install zoom"
 my:flatpak-install $ZOOM_APP_NAME
