9674df137988578ce5837b681183a562609f56a6
Yuri Pereira Constante <ypconstante@gmail.com>
2023-01-08 21:47:31 -0300

    Improve sound quality

--- shell/sound.sh
+++ shell/sound.sh
@@ -4,3 +4,7 @@ source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
 
 my:step-begin "configure sound"
 my:link-file "$ASSETS_DIR/sound--wireplumber.lua" "$XDG_CONFIG_HOME/wireplumber/main.lua.d/98-custom.lua"
+my:link-file "$ASSETS_DIR/sound--pipewire-client.conf" "$XDG_CONFIG_HOME/pipewire/client.conf.d/98-custom.conf"
+my:link-file "$ASSETS_DIR/sound--pipewire-pulse.conf" "$XDG_CONFIG_HOME/pipewire/pipewire-pulse.conf.d/98-custom.conf"
+
+systemctl --user restart pipewire.service pipewire-pulse.socket
