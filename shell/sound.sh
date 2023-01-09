33054db8f94cbb8d3e418fce99b457ea48378d4b
Yuri Pereira Constante <ypconstante@gmail.com>
2023-01-08 21:19:30 -0300

    Simplify wireplumber config

--- shell/sound.sh
+++ shell/sound.sh
@@ -3,7 +3,4 @@
 source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
 
 my:step-begin "configure sound"
-sudo cp -a /usr/share/wireplumber/main.lua.d/50-alsa-config.lua /etc/wireplumber/main.lua.d/50-alsa-config.lua
-
-# disable sound suspending, it causes clicking sounds
-sudo sed -Ei 's/^(\s+)\-+(.*session.suspend-timeout-seconds[^=]+=)[ 0-9]+(.*)$/\1\2 0\3/' /etc/wireplumber/main.lua.d/50-alsa-config.lua
+my:link-file "$ASSETS_DIR/sound--wireplumber.lua" "$XDG_CONFIG_HOME/wireplumber/main.lua.d/98-custom.lua"
