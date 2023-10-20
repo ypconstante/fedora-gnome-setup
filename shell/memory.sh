68249c273b068fefc408e3cd0e8c58e940280e7d
Yuri Pereira Constante <ypconstante@gmail.com>
2023-10-19 22:33:29 -0300

    Disable systemd-oomd

--- shell/memory.sh
+++ shell/memory.sh
@@ -5,3 +5,6 @@ source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
 my:step-begin "memory kernel parameters"
 my:copy-file "$ASSETS_DIR/memory--kernel-parameters.conf" /etc/sysctl.d/98-memory.conf
 sudo chmod 644 /etc/sysctl.d/98-memory.conf
+
+my:step-begin "disable systemd-oomd"
+sudo systemctl disable systemd-oomd.service
