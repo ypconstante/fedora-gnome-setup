65534cde8ca508bf8265bcb8a6ebbc5a7a715a8e
Yuri Pereira Constante <ypconstante@gmail.com>
2023-11-07 22:38:36 -0300

    Update node version

--- shell/dev_node.sh
+++ shell/dev_node.sh
@@ -4,7 +4,7 @@ source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
 
 my:step-begin "install"
 my:asdf-add-plugin nodejs
-my:asdf-install-and-set-global nodejs latest:18
+my:asdf-install-and-set-global nodejs latest:20
 
 my:step-begin "configure node"
 my:append-to-file-if-not-contains "$NPM_CONFIG_USERCONFIG" 'engine-strict = true'
