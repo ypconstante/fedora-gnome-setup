838768c09f5be788ad5989a0128094112fc10e9a
Yuri Pereira Constante <ypconstante@gmail.com>
2023-11-07 22:39:33 -0300

    Delete NetworkManager and resolved config

--- shell/network.sh
+++ shell/network.sh
@@ -2,12 +2,6 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
 
-my:step-begin "configure NetworkManager"
-my:copy-file "$ASSETS_DIR/network--NetworkManager.conf" /etc/NetworkManager/conf.d/98-fedora-setup.conf
-
-my:step-begin "configure systemd-resolved"
-my:copy-file "$ASSETS_DIR/network--systemd-resolved.conf" /etc/systemd/resolved.conf.d/98-fedora-setup.conf
-
 my:step-begin "configure firewall"
 sudo firewall-cmd --zone=public --permanent --remove-service ssh
 
