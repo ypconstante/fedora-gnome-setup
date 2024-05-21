005513956b54b7a3048dd4db5e3edd7150120e34
Yuri Pereira Constante <ypconstante@gmail.com>
2024-05-20 23:39:41 -0300

    Fix wireplumber config

--- shell/sound.sh
+++ shell/sound.sh
@@ -3,7 +3,7 @@
 source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
 
 my:step-begin "configure sound"
-my:link-file "$ASSETS_DIR/sound--wireplumber.lua" "$XDG_CONFIG_HOME/wireplumber/main.lua.d/98-custom.lua"
+my:link-file "$ASSETS_DIR/sound--wireplumber.conf" "$XDG_CONFIG_HOME/wireplumber/wireplumber.conf.d/98-custom.conf"
 my:link-file "$ASSETS_DIR/sound--pipewire-client.conf" "$XDG_CONFIG_HOME/pipewire/client.conf.d/98-custom.conf"
 my:link-file "$ASSETS_DIR/sound--pipewire-pulse.conf" "$XDG_CONFIG_HOME/pipewire/pipewire-pulse.conf.d/98-custom.conf"
 
