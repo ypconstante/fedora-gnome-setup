c3ace3954aba06015283144997ac2df3747005da
Yuri Pereira Constante <ypconstante@gmail.com>
2025-08-06 17:31:46 -0300

    Update vaapi setup

--- shell/graphics.sh
+++ shell/graphics.sh
@@ -10,13 +10,6 @@ my:dnf-install \
     vulkan-loader.i686 \
     vulkan-tools
 
-my:step-begin "enable vaapi"
-my:dnf-install \
-    gstreamer1-vaapi \
-    libva \
-    libva-utils \
-    vdpauinfo
-
 # https://rpmfusion.org/Howto/Multimedia
 my:step-begin "rpmfusion multimedia setup"
 sudo dnf swap --allowerasing -y -q ffmpeg-free ffmpeg
@@ -28,3 +21,12 @@ sudo dnf swap -y -q mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686
 
 my:step-begin "install gpu viewer"
 my:flatpak-install io.github.arunsivaramanneo.GPUViewer
+
+my:step-begin "enable vaapi"
+# https://fedoraproject.org/wiki/Firefox_Hardware_acceleration
+my:dnf-install \
+    libavcodec-freeworld \
+    mesa-va-drivers-freeworld \
+    libva-utils
+
+my:dnf-install gstreamer1-vaapi
