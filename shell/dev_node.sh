0b188afdc3b27252bcdc3aa9bd48e74c85fa5121
Yuri Pereira Constante <ypconstante@gmail.com>
2025-08-13 19:07:22 -0300

    Fix corepack config when mise is not in current shell

--- shell/dev_node.sh
+++ shell/dev_node.sh
@@ -4,4 +4,4 @@ source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
 
 my:step-begin "configure node"
 my:append-to-file-if-not-contains "$NPM_CONFIG_USERCONFIG" 'engine-strict = true'
-corepack enable
+mise exec node -- corepack enable
