98bee06c957e5d5362995f714a0d74f70fc953ad
Yuri Pereira Constante <ypconstante@gmail.com>
2025-08-13 15:38:17 -0300

    Remove zoom

--- shell/communication.sh
+++ shell/communication.sh
@@ -2,20 +2,5 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/_base.sh"
 
-ZOOM_APP_NAME=us.zoom.Zoom
-
 my:step-begin "install telegram"
 my:flatpak-install org.telegram.desktop
-
-my:step-begin "install zoom"
-my:flatpak-install $ZOOM_APP_NAME
-
-my:step-begin "configure zoom"
-flatpak override --user $ZOOM_APP_NAME --reset
-flatpak override --user $ZOOM_APP_NAME \
-    --nofilesystem=~/.zoom \
-    --persist=.zoom \
-    --nofilesystem=~/Documents/Zoom \
-    --persist=Documents
-rm -rf "$HOME/.zoom" "$HOME/Documents/Zoom"
-
