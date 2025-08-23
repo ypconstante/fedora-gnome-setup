544b5c9cb7fef21c0a2ab20c2cda2c9056df5eae
Yuri Pereira Constante <ypconstante@gmail.com>
2025-08-23 19:29:48 -0300

    Disable swap

--- shell/file-system_os.sh
+++ shell/file-system_os.sh
@@ -6,6 +6,9 @@ my:step-begin "file system kernel parameters"
 my:copy-file "$ASSETS_DIR/file-system--kernel-parameters.conf" /etc/sysctl.d/98-file-system.conf
 sudo chmod 644 /etc/sysctl.d/98-file-system.conf
 
+my:step-begin "disable swap"
+sudo swapoff -a
+
 my:step-begin "change mount options to optimize for ssd"
 sudo sed -Ei '/ssd/!s|( subvol[^ ]*)|\1,ssd|' /etc/fstab
 sudo sed -Ei '/noatime/!s|( subvol[^ ]*)|\1,noatime|' /etc/fstab
